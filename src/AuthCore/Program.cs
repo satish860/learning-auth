da680c693a39e49f680861af2a5bf9f875a1a6bb
Satish Venkatakrishnan <satish860@gmail.com>
2023-01-25 11:00:38 +0530

    Changed to use the Framework Default

--- src/AuthCore/Program.cs
+++ src/AuthCore/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.DataProtection;
+using System.Security.Claims;
 
 namespace AuthCore
 {
@@ -8,30 +11,74 @@ namespace AuthCore
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddDataProtection();
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddScoped<AuthService>();
+            builder.Services
+                   .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                   .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+
             var app = builder.Build();
 
+            //app.Use((context, next) =>
+            //{
+            //    var dataProtectionProvider = context.RequestServices.GetRequiredService<IDataProtectionProvider>();
+            //    var decryptor = dataProtectionProvider.CreateProtector("login");
+            //    var payload = context.Request.Cookies["auth"];
+            //    if (payload != null)
+            //    {
+            //        var decryptedstring = decryptor.Unprotect(payload);
+            //        var payloadParts = decryptedstring.Split(":");
+            //        var key = payloadParts[0];
+            //        var value = payloadParts[1];
+            //        var claims = new List<Claim>
+            //    {
+            //        new Claim("name",value)
+            //    };
+            //        var claimIdentity = new ClaimsIdentity(claims);
+            //        context.User = new System.Security.Claims.ClaimsPrincipal(claimIdentity);
+            //    }
+            //    return next();
+            //});
 
-            app.Map("/username", (HttpContext ctx,IDataProtectionProvider dataProtectionProvider) =>
+            app.UseAuthentication();
+
+            app.Map("/username", (HttpContext ctx, IDataProtectionProvider dataProtectionProvider) =>
             {
-                var decryptor = dataProtectionProvider.CreateProtector("login");
-                var payload = ctx.Request.Cookies["auth"];
-                var decryptedstring = decryptor.Unprotect(payload);
-                var payloadParts = decryptedstring.Split(":");
-                var key = payloadParts[0];
-                var value = payloadParts[1];
-                return value;
+                return ctx.User.Claims.FirstOrDefault(p => p.Type == "name").Value;
             });
 
-            app.Map("/login", (HttpContext ctx,IDataProtectionProvider provider) =>
+            app.Map("/login", async (HttpContext authService) =>
             {
-                var encryptor = provider.CreateProtector("login");
-                ctx.Response.Headers["set-cookie"] = $"auth={ encryptor.Protect("usr:satish")}";
+                var claims = new List<Claim>
+                {
+                   new Claim("name","satish")
+                };
+                var claimIdentity = new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);
+                var claimsPrincipal = new System.Security.Claims.ClaimsPrincipal(claimIdentity);
+                await authService.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,claimsPrincipal);
                 return "ok";
             });
 
             app.MapGet("/", () => "Hello World!");
 
             app.Run();
+        }
+    }
+
+    public class AuthService
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly IDataProtectionProvider dataProtectionBuilder;
+
+        public AuthService(IHttpContextAccessor httpContextAccessor, IDataProtectionProvider dataProtectionBuilder)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+            this.dataProtectionBuilder = dataProtectionBuilder;
+        }
+        public void SignIn()
+        {
+            var protector = dataProtectionBuilder.CreateProtector("login");
+            this.httpContextAccessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:satish")}";
         }
     }
 }

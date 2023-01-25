b562c3326f7cef2f55dcf642dacb84c7c760a977
Satish Venkatakrishnan <satish860@gmail.com>
2023-01-25 10:03:59 +0530

    Updated the code with Data Protection

--- src/AuthCore/Program.cs
+++ src/AuthCore/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.DataProtection;
+
 namespace AuthCore
 {
     public class Program
@@ -5,7 +7,27 @@ namespace AuthCore
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDataProtection();
             var app = builder.Build();
+
+
+            app.Map("/username", (HttpContext ctx,IDataProtectionProvider dataProtectionProvider) =>
+            {
+                var decryptor = dataProtectionProvider.CreateProtector("login");
+                var payload = ctx.Request.Cookies["auth"];
+                var decryptedstring = decryptor.Unprotect(payload);
+                var payloadParts = decryptedstring.Split(":");
+                var key = payloadParts[0];
+                var value = payloadParts[1];
+                return value;
+            });
+
+            app.Map("/login", (HttpContext ctx,IDataProtectionProvider provider) =>
+            {
+                var encryptor = provider.CreateProtector("login");
+                ctx.Response.Headers["set-cookie"] = $"auth={ encryptor.Protect("usr:satish")}";
+                return "ok";
+            });
 
             app.MapGet("/", () => "Hello World!");
 
